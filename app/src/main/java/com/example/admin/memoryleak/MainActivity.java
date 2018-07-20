48b5c4a7bfae53894e6b569705b43d317b39e068
Mario Alberto Garcia Coronel <Mario.coronel@mobileappscompany.us>
2018-07-20 12:03:11 -0400

    PDF intent defined

--- app/src/main/java/com/example/admin/memoryleak/MainActivity.java
+++ app/src/main/java/com/example/admin/memoryleak/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.admin.memoryleak;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,8 +38,21 @@ public class MainActivity extends AppCompatActivity {
         btnRender.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, PDFActivity.class);
-                startActivity(intent);
+                Intent target = new Intent(Intent.ACTION_VIEW);
+                Uri doc = Uri.parse("/app/src/main/res/raw/dantesinferno.pdf");
+                File pdftoread = new File("app/src/main/res/raw/dantesinferno.pdf");
+                Toast.makeText(MainActivity.this, doc.getPath(), Toast.LENGTH_SHORT).show();
+                target.setDataAndType(Uri.fromFile(pdftoread),"application/pdf");
+                target.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                Intent intent = Intent.createChooser(target, "Open File");
+                try {
+                    startActivity(intent);
+                } catch (ActivityNotFoundException e) {
+                    // Instruct the user to install a PDF reader here, or something
+                }
+
+
+
             }
         });
 
